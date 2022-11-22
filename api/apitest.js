9dd23cb7095a3c8538841842078f088da1f7bae4
Oscar Alviarez <oscalviarez@gmail.com>
2022-11-22 12:21:00 -0300

    apitest

---
+++ api/apitest.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    color: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dxfksb8ua/image/upload/v1667593738/fdk1ul9ryzcefuyrb9zb.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dxfksb8ua/image/upload/v1667593962/kd77yztq2qjmizap8ojh.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dxfksb8ua/image/upload/v1667594144/oz3wegyq6rqsvf2t4xdx.jpg",
+  },
+];
+
+module.exports = products;
