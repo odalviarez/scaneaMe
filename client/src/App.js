d3ce1bd82d5d4e6780927cae1bbd2c84ef3af377
jonasojeda <jonojed98@gmail.com>
2022-11-22 18:29:36 -0300

    creacion de rutas
    creacion de rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {Route, Switch} from 'react-router'
+//import { Counter } from './features/counter/Counter';
+import Home from './pages/home/Home';
+import Catalogue from './pages/catalogue/Catalogue'
+import ContactForm from './pages/contactForm/ContactForm'
+import Cart from './pages/cart/Cart'
+import Detail from './pages/detail/Detail'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path={"/home"} component={Home}/>
+        <Route exact path={"/catalogue"} component={Catalogue}/>
+        <Route exact path={"/detail"} component={Detail}/>
+        <Route exact path={"/cart"} component={Cart}/>
+        <Route exact path={"/contact"} component={ContactForm}/>
+      </Switch>
     </div>
   );
 }
