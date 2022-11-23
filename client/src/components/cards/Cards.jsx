4200c647bde34c802d458443ad06907b09f3a5b6
gzucca <gonzalozucca_4@outlook.com>
2022-11-23 12:09:48 -0300

    Cambios en el catalogo

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import products from '../../productos'
+import Card from '../card/Card'
 
 export default function Cards() {
+
+  // const dispatch = useDispatch();
+
+  // const [state, setstate] = useState(initialState);
+
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   };
+  // }, [input]);
+
+
+
+
+
+
+
+
   return (
-    <div>Cards</div>
+    <div>
+    
+      <div>
+        {products? products.map(p => {
+          return <Card
+          key={p.id}
+          id={p.id}
+          name={p.name}
+          img={p.image}/>
+        }) : 'No product was found'}
+      </div>
+    
+    
+    
+    </div>
+
   )
 }
