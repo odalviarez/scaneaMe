07c0924ff6b55c3ca6f91ed679773ffc8483db35
Octa <navarrooctavioa@gmail.com>
2022-11-23 22:41:20 -0300

    Carrusel terminado

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../../components/navBar/NavBar.jsx'
+import Carru from '../../components/homeBanners/HomeBanners'
 
 export default function Home() {
   return (
     <div>
       <NavBar />
+      <Carru />
     </div>
   )
 }
