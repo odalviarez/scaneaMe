095ba8d4ba4127026179df911c62544a670aac3a
jonasojeda <jonojed98@gmail.com>
2022-11-24 10:43:22 -0300

    coneccion card detail

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -11,7 +11,7 @@ export default function Card({name, img, id, handleAddCart}) {
 
   return (
     <div  >
-        <Link to={`/detail`}><h2>{name}</h2></Link>
+        <Link to={`/detail:${id}`}><h2>{name}</h2></Link>
         
         <img height="400px" src={img} alt="imagen" />
 
--- client/src/pages/detail/Detail.jsx
+++ client/src/pages/detail/Detail.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import products from '../../productos'
 import NavBar from '../../components/navBar/NavBar.jsx'
 import Details from '../../components/details/Details.jsx'
 
 export default function Detail() {
+  const dispatch = useDispatch();
+    const productDetail = useSelector(state => state.productDetail);
+    const { id } = useParams();
+    console.log(id)
   return (
     <div>
       <NavBar />
