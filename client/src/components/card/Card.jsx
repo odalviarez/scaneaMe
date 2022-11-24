af646902d7700a35c38a8efb0e448337cedeb57a
jonasojeda <jonojed98@gmail.com>
2022-11-24 18:15:38 -0300

    agregado de estilo

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -11,7 +11,7 @@ export default function Card({name, img, id, price, type, color, handleAddCart})
 
 
   return (
-    <div  >
+    <div className={styles.card} >
         <Link to={`/detail/${id}`}><h2>{name}</h2></Link>
 
         
