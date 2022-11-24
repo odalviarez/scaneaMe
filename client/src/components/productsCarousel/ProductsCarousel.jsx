a7d7428e76dd645b3f6ac5db6db985faec4dd25b
Octa <navarrooctavioa@gmail.com>
2022-11-24 18:22:33 -0300

    Carrusel de productos

---
+++ client/src/components/productsCarousel/ProductsCarousel.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import styles from './ProductsCarousel.module.css'
+import { getAllProducts } from '../../redux/actions'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import HomeCard from './HomeCard'
+
+export default function HomeBanners() {
+  const dispatch = useDispatch()
+
+  const allProducts = useSelector(state => state.allProducts)
+  const product = allProducts.map(e => ({ image: e.image, price: e.price, id: e.id }))
+
+  useEffect(() => {
+    if (allProducts.length === 0) {
+      dispatch(getAllProducts())
+    }
+  }, [])
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  }
+
+  const productCard = product.map(item => (
+    <HomeCard image={item.image} price={item.price} id={item.id} />
+  ))
+
+  return (
+    <div>
+      <Carousel responsive={responsive}>{productCard}</Carousel>
+    </div>
+  )
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../../components/navBar/NavBar.jsx'
 import Carru from '../../components/homeBanners/HomeBanners'
+import ProdCarru from '../../components/productsCarousel/ProductsCarousel'
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <Carru />
+      <ProdCarru />
     </div>
   )
 }
