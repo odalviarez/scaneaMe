e92a732ad068346d74d790ff8c3a9017d508d221
Octa <navarrooctavioa@gmail.com>
2022-11-24 21:35:01 -0300

    Bosquejo de home

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../../components/navBar/NavBar.jsx'
 import Carru from '../../components/homeBanners/HomeBanners'
 import ProdCarru from '../../components/productsCarousel/ProductsCarousel'
+import Footer from '../../components/footer/Footer'
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
       <NavBar />
       <Carru />
       <ProdCarru />
+      <ProdCarru />
+      <ProdCarru />
+      <Footer />
     </div>
   )
 }
