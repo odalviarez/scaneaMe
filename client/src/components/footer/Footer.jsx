d7e16fdc6e1c34e2fb93d49da988a71d67bbdf14
Octa <navarrooctavioa@gmail.com>
2022-12-06 20:54:22 -0300

    Home mejorado, mas responsive

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -13,76 +13,64 @@ export default function Footer() {
   let year = date.getFullYear()
   return (
     <div className={styles.container}>
-      <Container fluid className='footer'>
-        <Row>
-          <div className={styles.designed}>
-            <p>Designed and Developed by: scaneaMe Team</p>
-          </div>
-          <div md='4' className={styles.copyright}>
-            <p>Copyright © {year} SCANEAME</p>
-          </div>
-          <Col md='4' className='footer-body'>
-            <ul className='footer-icons'>
-              <li className='social-icons'>
-                <a
-                  href='https://github.com/scanneame'
-                  style={{ color: 'white' }}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src={githubLogo}
-                    alt='Github link'
-                    className={styles.githubLogo}
-                  />
-                </a>
-              </li>
-              <li className='liVacio'>
-                <a
-                  href='https://twitter.com/Scannme_'
-                  style={{ color: 'white' }}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src={twLogo}
-                    alt='Twitter link'
-                    className={styles.twLogo}
-                  />
-                </a>
-              </li>
-              <li className='liVacio'>
-                <a
-                  href='https://www.linkedin.com/in/scannme-team/'
-                  style={{ color: 'white' }}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src={linkedinLogo}
-                    alt='LinkedIn link'
-                    className={styles.liLogo}
-                  />
-                </a>
-              </li>
-              <li className='liVacio'>
-                <a
-                  href='https://www.instagram.com/scanneameqr/'
-                  style={{ color: 'white' }}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src={igLogo}
-                    alt='Instagram link'
-                    className={styles.igLogo}
-                  />
-                </a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+      <div className={styles.textContainer}>
+        <div className={styles.designed}>
+          <p>Designed and Developed by: scaneaMe Team</p>
+        </div>
+        <div className={styles.copyright}>
+          <p>Copyright © {year} SCANEAME</p>
+        </div>
+      </div>
+      <ul className='footer-icons'>
+        <li className='social-icons'>
+          <a
+            href='https://github.com/scanneame'
+            style={{ color: 'white' }}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={githubLogo}
+              alt='Github link'
+              className={styles.Logo}
+            />
+          </a>
+        </li>
+        <li className='liVacio'>
+          <a
+            href='https://twitter.com/Scannme_'
+            style={{ color: 'white' }}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img src={twLogo} alt='Twitter link' className={styles.Logo} />
+          </a>
+        </li>
+        <li className='liVacio'>
+          <a
+            href='https://www.linkedin.com/in/scannme-team/'
+            style={{ color: 'white' }}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img
+              src={linkedinLogo}
+              alt='LinkedIn link'
+              className={styles.Logo}
+            />
+          </a>
+        </li>
+        <li className='liVacio'>
+          <a
+            href='https://www.instagram.com/scanneameqr/'
+            style={{ color: 'white' }}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img src={igLogo} alt='Instagram link' className={styles.Logo} />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
