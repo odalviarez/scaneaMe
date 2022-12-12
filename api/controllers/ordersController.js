3a8eb15cfe755e458f92a6e7db9544d53b9137df
Oscar Alviarez <oscalviarez@gmail.com>
2022-12-11 21:31:08 -0300

    Update ordersController.js

--- api/controllers/ordersController.js
+++ api/controllers/ordersController.js
@@ -1,4 +1,4 @@
-const { Order } = require("../models/orderModel");
+const Order = require("../models/orderModel");
 //const { auth, isUser, isAdmin } = require("../middleware/auth");
 
 const router = require("express").Router();
