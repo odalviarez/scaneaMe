50dd6d7155188221ac4e23f5701512a0a9937363
Oscar Alviarez <oscalviarez@gmail.com>
2022-12-14 19:23:24 -0300

    Feature/DeleteSocials

--- client/src/components/userAccComp/UserAccComp.jsx
+++ client/src/components/userAccComp/UserAccComp.jsx
@@ -1,37 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { userUpdate, getUser, userUpdateAuth0 } from '../../redux/actions'
-import style from './UserAccComp.module.css'
-import { useAuth0 } from '@auth0/auth0-react'
-import validator from 'validator';
-import tlds from 'tld-list'
-import Popup from 'reactjs-popup';
-var passwordValidator = require('password-validator');
-
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userUpdate, getUser, userUpdateAuth0 } from "../../redux/actions";
+import style from "./UserAccComp.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import validator from "validator";
+import tlds from "tld-list";
+import Popup from "reactjs-popup";
+var passwordValidator = require("password-validator");
 
 export default function UserAccComp() {
-  const { user, getAccessTokenSilently } = useAuth0()
-  const dispatch = useDispatch()
-  const userLogin = useSelector(state => state.userLogin);
+  const { user, getAccessTokenSilently } = useAuth0();
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
   // eslint-disable-next-line no-unused-vars
-  const [errors, setErrors] = useState({ 
+  const [errors, setErrors] = useState({
     email: "",
     password: [],
-    passwordRepeat: ""
-  })
-  const [image, setImage] = useState('')
-    // eslint-disable-next-line no-unused-vars
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordRepeat, setPasswordRepeat] = useState('')
-  const [aboutUser, setAboutUser] = useState('')
+    passwordRepeat: "",
+  });
+  const [image, setImage] = useState("");
+  // eslint-disable-next-line no-unused-vars
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRepeat, setPasswordRepeat] = useState("");
+  const [aboutUser, setAboutUser] = useState("");
   const [socials, setSocials] = useState({
-    facebook: '',
-    linkedin: '',
-    twitter: '',
-    instagram: '',
-  })
+    facebook: "",
+    linkedin: "",
+    twitter: "",
+    instagram: "",
+  });
 
   const getToken = async () => {
     const token = await getAccessTokenSilently();
@@ -39,79 +37,82 @@ export default function UserAccComp() {
   };
 
   useEffect(() => {
-    if (Object.hasOwn(userLogin, 'socials')) setSocials(userLogin.socials)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, user])
+    if (Object.hasOwn(userLogin, "socials") && user) setSocials(userLogin.socials);
+
+
+  }, [dispatch, user]);
 
   useEffect(() => {
     return () => {
-      dispatch(getUser(userLogin.email))
+      dispatch(getUser(userLogin.email));
     };
-
   }, [dispatch, userLogin.email]);
 
-  
-
-
   const validateEmail = (e) => {
-    e.preventDefault(e)
-  
-    if (validator.isEmail(email, {domain_specific_validation: true}) === false || tlds.includes(email.split('.').pop()) === false) {
+    e.preventDefault(e);
+
+    if (
+      validator.isEmail(email, { domain_specific_validation: true }) ===
+        false ||
+      tlds.includes(email.split(".").pop()) === false
+    ) {
       setErrors({
         ...errors,
-        email : 'Email is not valid'
-    })} else if (errors.email.length === 0) {
-      
-      dispatch(userUpdateAuth0(email, userLogin.sub, 'emailChange', getToken))
-      alert("User email updated")
+        email: "Email is not valid",
+      });
+    } else if (errors.email.length === 0) {
+      dispatch(userUpdateAuth0(email, userLogin.sub, "emailChange", getToken));
+      alert("User email updated");
     }
-  }
-
-  
+  };
 
   let schema = new passwordValidator();
-  schema.is().min(8, 'Password must be at least 8 characters long')
-  schema.is().max(20, 'Password must be less than 20 characters long')
-  schema.has().uppercase(1, 'Password must have at least 1 uppercase letter')
-  schema.has().lowercase(1, 'Password must have at least 1 lowercase letter')
-  schema.has().digits(1, 'Password must have at least 1 number')
-  schema.has().symbols(1, 'Password must have at least 1 special character')
-  schema.has().not().spaces(1, 'Password must not have spaces')
-
+  schema.is().min(8, "Password must be at least 8 characters long");
+  schema.is().max(20, "Password must be less than 20 characters long");
+  schema.has().uppercase(1, "Password must have at least 1 uppercase letter");
+  schema.has().lowercase(1, "Password must have at least 1 lowercase letter");
+  schema.has().digits(1, "Password must have at least 1 number");
+  schema.has().symbols(1, "Password must have at least 1 special character");
+  schema.has().not().spaces(1, "Password must not have spaces");
 
   const validatePassword = async (e) => {
-    e.preventDefault(e)
+    e.preventDefault(e);
 
     if (password !== passwordRepeat) {
       setErrors({
         ...errors,
-        password : schema.validate(password, {'details': true}).map(e => e.message),
-        passwordRepeat : 'Passwords do not match'
-      })} else {
+        password: schema
+          .validate(password, { details: true })
+          .map((e) => e.message),
+        passwordRepeat: "Passwords do not match",
+      });
+    } else {
       setErrors({
         ...errors,
-        password : schema.validate(password, {'details': true}).map(e => e.message),
-        passwordRepeat : '',
-      })
-      }
+        password: schema
+          .validate(password, { details: true })
+          .map((e) => e.message),
+        passwordRepeat: "",
+      });
+    }
 
     if (errors.password.length === 0 && errors.passwordRepeat.length === 0) {
-      
-      dispatch(userUpdateAuth0(password, userLogin.sub, 'passwordChange', getToken))
-      alert("User password updated")
+      dispatch(
+        userUpdateAuth0(password, userLogin.sub, "passwordChange", getToken)
+      );
+      alert("User password updated");
     }
-  }
+  };
 
-
-  const handleChangeSocials = e => {
+  const handleChangeSocials = (e) => {
     setSocials({
       ...socials,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
   const handleSubmitProfile = async (e) => {
-    e.preventDefault(e)
+    e.preventDefault(e);
 
     dispatch(
       userUpdate(
@@ -123,19 +124,19 @@ export default function UserAccComp() {
         userLogin.email,
         getToken
       )
-    )
-    alert("User information updated")
-  }
+    );
+    alert("User information updated");
+  };
 
   const handleDeleteAccount = async (e) => {
-    e.preventDefault(e)
+    e.preventDefault(e);
 
-    dispatch(userUpdateAuth0(null, userLogin.sub, 'delete', getToken))
-    window.location.href = window.location.origin + "/home"
-  }
+    dispatch(userUpdateAuth0(null, userLogin.sub, "delete", getToken));
+    window.location.href = window.location.origin + "/home";
+  };
 
   const showPassword = (e) => {
-    e.preventDefault(e)
+    e.preventDefault(e);
     var x = document.getElementById("passwordInput");
     var y = document.getElementById("passwordInputRepeat");
     if (x.type === "password") {
@@ -145,101 +146,133 @@ export default function UserAccComp() {
       x.type = "password";
       y.type = "password";
     }
-      
-  }
+  };
 
-  const handleImage = e => {
-    const file = e.target.files[0]
+  const handleImage = (e) => {
+    const file = e.target.files[0];
     if (file.size < 10000000) {
-    setFileToBase(file)
+      setFileToBase(file);
     } else {
-      alert ('The image file size should be smaller than 10mb')
+      alert("The image file size should be smaller than 10mb");
     }
-  }
+  };
 
-  const setFileToBase = file => {
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
+  const setFileToBase = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
     reader.onloadend = () => {
-      setImage(reader.result)
-    }
-  }
+      setImage(reader.result);
+    };
+  };
 
+  const deleteSocials = (e) => {
+    e.preventDefault();
+    setSocials({
+      facebook: null,
+      linkedin: null,
+      twitter: null,
+      instagram: null,
+    });
+    dispatch(
+      userUpdate(
+        {
+          socials,
+        },
+        userLogin.email,
+        getToken
+      )
+    );
+    alert("User socials deleted")
+  };
 
   return (
     <div className={style.UserAccCompContainer}>
       <h1>User Account Info</h1>
 
-      {userLogin?.sub?.includes('auth0')? 
-      
-      <div> 
-        <div className={style.UserAccCompItem}>
-          <label>Email:</label>
-          <input type='email' value={userLogin?.email} disabled />
-        </div>
-        <form onSubmit={e => validateEmail(e)} className={style.UserAccCompItem} >
-          <label>Change Email:</label>
-          <input type='email' onChange={e => setEmail(e.target.value)} />
-          <button type='submit'>SUBMIT</button>
-        </form>
-        {errors.email? (<p>{errors.email}</p>) : "" }
-        <form onSubmit={e => validatePassword(e)}>
+      {userLogin?.sub?.includes("auth0") ? (
+        <div>
           <div className={style.UserAccCompItem}>
-            <label>New password:</label>
-            <input id='passwordInput' type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+            <label>Email:</label>
+            <input type="email" value={userLogin?.email} disabled />
           </div>
-          {errors.password?.map(e => {
-            return (<p key={e}>{e}</p>)})}
-          <div className={style.UserAccCompItem}>
-            <label>Repeat password:</label>
-            <input id='passwordInputRepeat' type='password' value={passwordRepeat} onChange={e => setPasswordRepeat(e.target.value)} />
-            <button type='submit'>SUBMIT</button>
-          </div>
-          <div className={style.showPassword}>
-            <button  type="checkbox" onClick={(e) => showPassword(e)}>SHOW</button>
-          </div>
-            {errors.passwordRepeat? (<p>{errors.passwordRepeat}</p>) : "" }
-        </form>
-      </div>
-    
-      : 
-
-      <div> 
-        <div className={style.UserAccCompItem}>
-          <label>Email:</label>
-          <input type='email' value={userLogin?.email} disabled />
+          <form
+            onSubmit={(e) => validateEmail(e)}
+            className={style.UserAccCompItem}
+          >
+            <label>Change Email:</label>
+            <input type="email" onChange={(e) => setEmail(e.target.value)} />
+            <button type="submit">SUBMIT</button>
+          </form>
+          {errors.email ? <p>{errors.email}</p> : ""}
+          <form onSubmit={(e) => validatePassword(e)}>
+            <div className={style.UserAccCompItem}>
+              <label>New password:</label>
+              <input
+                id="passwordInput"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            {errors.password?.map((e) => {
+              return <p key={e}>{e}</p>;
+            })}
+            <div className={style.UserAccCompItem}>
+              <label>Repeat password:</label>
+              <input
+                id="passwordInputRepeat"
+                type="password"
+                value={passwordRepeat}
+                onChange={(e) => setPasswordRepeat(e.target.value)}
+              />
+              <button type="submit">SUBMIT</button>
+            </div>
+            <div className={style.showPassword}>
+              <button type="checkbox" onClick={(e) => showPassword(e)}>
+                SHOW
+              </button>
+            </div>
+            {errors.passwordRepeat ? <p>{errors.passwordRepeat}</p> : ""}
+          </form>
         </div>
-      </div>
-      }
+      ) : (
+        <div>
+          <div className={style.UserAccCompItem}>
+            <label>Email:</label>
+            <input type="email" value={userLogin?.email} disabled />
+          </div>
+        </div>
+      )}
 
-
-      <form onSubmit={e => handleSubmitProfile(e)}>
+      <form onSubmit={(e) => handleSubmitProfile(e)}>
         <div className={style.UserAccCompAbout}>
           <label>About me:</label>
-          <textarea 
-          type='text' 
-          maxLength="255"
-          rows='5'
-          placeholder={userLogin.info && userLogin.info}
-          value={aboutUser} 
-          onChange={(e) => setAboutUser(e.target.value)}
+          <textarea
+            type="text"
+            maxLength="255"
+            rows="5"
+            placeholder={userLogin.info && userLogin.info}
+            value={aboutUser}
+            onChange={(e) => setAboutUser(e.target.value)}
           />
         </div>
+
         <div className={style.UserAccCompItem}>
           <label>Instagram:</label>
           <input
-            type='text'
-            name='instagram'
+            type="text"
+            name="instagram"
             value={socials?.instagram}
             onChange={handleChangeSocials}
             placeholder={userLogin?.socials?.instagram}
           />
         </div>
+
         <div className={style.UserAccCompItem}>
           <label>Facebook:</label>
           <input
-            type='text'
-            name='facebook'
+            type="text"
+            name="facebook"
             value={socials?.facebook}
             onChange={handleChangeSocials}
             placeholder={userLogin?.socials?.facebook}
@@ -248,8 +281,8 @@ export default function UserAccComp() {
         <div className={style.UserAccCompItem}>
           <label>LinkedIn:</label>
           <input
-            type='text'
-            name='linkedin'
+            type="text"
+            name="linkedin"
             value={socials?.linkedin}
             onChange={handleChangeSocials}
             placeholder={userLogin?.socials?.linkedin}
@@ -258,8 +291,8 @@ export default function UserAccComp() {
         <div className={style.UserAccCompItem}>
           <label>Twitter:</label>
           <input
-            type='text'
-            name='twitter'
+            type="text"
+            name="twitter"
             value={socials?.twitter}
             onChange={handleChangeSocials}
             placeholder={userLogin?.socials?.twitter}
@@ -270,27 +303,51 @@ export default function UserAccComp() {
           <div>Update Image:</div>
           <input
             onChange={handleImage}
-            type='file'
-            id='formupload'
-            name='image'
+            type="file"
+            id="formupload"
+            name="image"
             accept=".png, .jpg, .jpeg"
             className={style.imgUpload}
-            placeholder='Select file...'
+            placeholder="Select file..."
           />
         </div>
-        <button
-          type='text'
-          className={style.submitProfile}
-        >
-          SUBMIT
-        </button>
+
+        <div>
+          <p></p>
+          <button
+            type="text"
+            class="py-2 px-4 text-sm font-medium text-gray-900 rounded-lg border bg-white/30 border-gray-700 hover:backdrop-blur-sm hover:bg-white/50 hover:text-slate-700 focus:z-10 focus:ring-2 focus:ring-slate-700 focus:text-slate-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-slate-500 dark:focus:text-white"
+          >
+            SUBMIT
+          </button>
+        </div>
       </form>
-    <Popup  trigger={<button>DELETE ACCOUNT</button>} position="right center">
-      <div className={style.popupDelete}>
-        <p>Delete account? This action cannot be reversed.</p>
-        <button className={style.submitProfile} onClick={handleDeleteAccount}> DELETE </button>
+      <p></p>
+      <div>
+        <button
+          onClick={deleteSocials}
+          class="py-2 px-4 text-sm font-medium text-gray-900 rounded-lg border bg-white/30 border-gray-700 hover:backdrop-blur-sm hover:bg-white/50 hover:text-slate-700 focus:z-10 focus:ring-2 focus:ring-slate-700 focus:text-slate-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-slate-500 dark:focus:text-white"
+        >
+          DELETE SOCIALS
+        </button>
       </div>
-    </Popup>
+      <p></p>
+      <Popup
+        trigger={
+          <button class="py-2 px-4 text-sm font-medium text-gray-900 rounded-lg border bg-white/30 border-gray-700 hover:backdrop-blur-sm hover:bg-white/50 hover:text-slate-700 focus:z-10 focus:ring-2 focus:ring-slate-700 focus:text-slate-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-slate-500 dark:focus:text-white">
+            DELETE ACCOUNT
+          </button>
+        }
+        position="right center"
+      >
+        <div className={style.popupDelete}>
+          <p>Delete account? This action cannot be reversed.</p>
+          <button className={style.submitProfile} onClick={handleDeleteAccount}>
+            {" "}
+            DELETE{" "}
+          </button>
+        </div>
+      </Popup>
     </div>
-  )
+  );
 }
